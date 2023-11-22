c74d63708984cda721496910a56e2684b4c392fa
Anastasiya <nastya.kuzich.02@mail.ru>
2023-11-22 17:51:32 +0200

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,134 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+const colors = require("tailwindcss/colors");
+const plugin = require("tailwindcss/plugin");
+
+const primary = "#e30b13";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      primary,
+      black: colors.black,
+      white: colors.white,
+      transparent: colors.transparent,
+      yellow: {
+        700: "#f5c521",
+      },
+      gray: {
+        300: "#d9dae8",
+        500: "#999aa5",
+        600: "#66676e",
+        700: "#39393f",
+        800: "#242529",
+        900: "#19181f",
+        950: "#101215",
+      },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      spacing: {
+        0.5: "0.12rem",
+        layout: "2.75rem",
+      },
+      fontSize: {
+        "2lg": "1.38rem",
+      },
+      borderRadius: {
+        image: "0.5rem",
+        layout: "0.8rem",
+      },
+      transitionTimingFunction: {
+        DEFAULT: "ease-in-out",
+      },
+      transitionDuration: {
+        DEFAULT: "200ms",
+      },
+      zIndex: {
+        1: "1",
+        2: "2",
+        3: "3",
+      },
+      keyframes: {
+        fade: {
+          from: { opacity: "0" },
+          to: { opacity: "1" },
+        },
+        scaleIn: {
+          "0%": {
+            opacity: "0",
+            transform: "scale(0.9)",
+          },
+          "50%": {
+            opacity: "0.3",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "scale(1)",
+          },
+        },
+      },
+      animation: {
+        fade: " fade .5s ease-in-out",
+        scaleIn: " scaleIn .35s ease-in-out",
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [
+    plugin(({ addComponents, theme, addUtilities }) => {
+      addComponents({
+        ".btn-primary": {
+          backgrountColor: primary,
+          color: "#fff",
+          baorderRadius: "0.65rem",
+          transition: "backgrount-color .3s ease-in-out",
+          "&:hover": {
+            backgrountColor: "#ff0009",
+          },
+        },
+        "text-link": {
+          textUnderLineOffset: 4,
+          color: "rgba(255, 255, 255, .9)",
+          transition: "text-decoration-color .3s ease-in-out",
+          textDecorationLine: "underline",
+          textDecorationColor: "rgba(255, 255, 255, .2)",
+          "&:hover": {
+            textDecorationColor: "rgba(255, 255, 255, .9)",
+          },
+        },
+        ".air-block": {
+          borderRadius: theme("borderRadius.layout"),
+          backgroundColor: theme("colors.gray.950"),
+          color: theme("colors.white"),
+          boxShadow: theme("boxShadow.lg"),
+        },
+      });
+      addUtilities({
+        ".text-shadow": {
+          textShadow: "1px 1px rgba(0, 0, 0, 0.4)",
+        },
+
+        ".outline-border-none": {
+          outline: "none",
+          border: "none",
+        },
+
+        ".flex-center-between": {
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        },
+
+        ".image-like-bg": {
+          objectPosition: "center",
+          objectFit: "cover",
+          pointerEvents: "none",
+        },
+      });
+    }),
+  ],
+};
+export default config;
